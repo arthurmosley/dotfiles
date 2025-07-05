1d1bdb409878817908e145129873c77777af4876
Arthur Mosley <arthucharlesmosley@gmail.com>
2025-07-05 17:57:43 -0400

    deleting a lot of cahced downloaded stuff

--- elpa/base16-theme-20250609.1645/base16-theme-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "base16-theme" "20250609.1645"
-  "Collection of themes built on combinations of 16 base colors."
-  ()
-  :url "https://github.com/tinted-theming/base16-emacs"
-  :commit "ad30917e50ea062e0d9ffb378a03717b386409bd"
-  :revdesc "ad30917e50ea"
-  :authors '(("Kaleb Elwert" . "belak@coded.io"))
-  :maintainers '(("Kaleb Elwert" . "belak@coded.io")))
--- elpa/jazz-theme-20230814.1916/jazz-theme-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "jazz-theme" "20230814.1916"
-  "A warm color theme for Emacs 24+."
-  ()
-  :url "https://github.com/donderom/jazz-theme"
-  :commit "b936b392e3ea3b6968530e3d5e5fccb9c454b5f8"
-  :revdesc "b936b392e3ea"
-  :authors '(("Roman Parykin" . "donderom@ymail.com"))
-  :maintainers '(("Roman Parykin" . "donderom@ymail.com")))
