32b819373f9a44d29e23e396193b47345264094c
Arthur Mosley <arthurcharlesmosley@gmail.com>
2026-02-15 19:24:59 -0500

    latest emacs configuration

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; *-* lexical-binding: t -*-
+;; -*- lexical-binding: t -*-
 
 (setopt custom-file "~/.emacs.custom")
 (load custom-file)
@@ -6,29 +6,36 @@
 ;; Initialize package
 (setopt package-native-compile t)
 (setopt package-archives
-	'(("gnu" . "http://elpa.gnu.org/packages/")
-	  ("gnu-devel" . "https://elpa.gnu.org/devel/")
-	  ("nongnu" . "http://elpa.nongnu.org/nongnu/")
-	  ("melpa" . "http://melpa.org/packages/")))
+    '(("gnu" . "http://elpa.gnu.org/packages/")
+      ("gnu-devel" . "https://elpa.gnu.org/devel/")
+      ("nongnu" . "http://elpa.nongnu.org/nongnu/")
+      ("melpa" . "http://melpa.org/packages/")))
 
 (use-package exec-path-from-shell
   :init
   (when (memq window-system '(x pgtk))
     (exec-path-from-shell-initialize)))
 
-(setenv "PYTHON_BASIC_REPL" "1")
-(add-to-list 'process-environment "PYTHON_BASIC_REPL=1")
 (add-to-list 'exec-path "/home/linuxbrew/.linuxbrew/bin")
 (add-to-list 'exec-path "/usr/local/texlive/2025/bin/")
+(add-to-list 'exec-path "/home/arthurmosley/.bun/bin")
+
+(setq use-package-always-ensure t)
+
+;; Disable all themes before loading a new one
+(mapc #'disable-theme custom-enabled-themes)
+(load-theme 'doom-one)
 
 ;; early in init.el
 (use-package emacs
   :ensure nil
+  :init
+  (require-theme 'modus-themes)
   :config
   (add-hook 'prog-mode-hook #'flymake-mode)
   (add-hook 'prog-mode-hook #'display-line-numbers-mode)
+  (define-key global-map (kbd "<f5>") #'modus-themes-toggle)
   :custom
-  ;; not sure if i want this yet
   (tab-always-indent t)
   (context-menu-mode t)
   (enable-recursive-minibuffers t)
@@ -36,13 +43,37 @@
   (minibuffer-prompt-properties
    '(read-only t cursor-intangible t face minibuffer-prompt)))
 
+(set-face-attribute 'default nil
+                    :font "Monaspace Neon"   ; paid, beautiful
+                    ;; alternatives:
+                    ;; "Iosevka"            ; free, very customizable
+                    ;; "Monaspace Neon"     ; free, Microsoft, ligature support
+                    ;; "Maple Mono"         ; free, great ligatures
+                    :height 120            ; 130 = 13pt, tweak to taste
+                    :weight 'regular)
+
+(setq-default line-spacing 4)
+(fringe-mode '(8 . 8))
+(show-paren-mode 1)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; ----------------------- TREESITTER SETUP ----------------------- ;;
 (use-package treesit-auto
   :custom
-  (treesit-auto-install 't) ;; Auto install without asking
+  (treesit-auto-install 't)
   :config
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
 
+(setq treesit-language-source-alist
+      '((typescript . ("https://github.com/tree-sitter/tree-sitter-typescript"
+                       "master" "typescript/src"))
+        (tsx        . ("https://github.com/tree-sitter/tree-sitter-typescript"
+                       "master" "tsx/src"))))
+
+;; Remap major modes to treesitter versions
 (setq major-mode-remap-alist
       '((python-mode     . python-ts-mode)
         (clojure-mode    . clojure-ts-mode)
@@ -62,64 +93,42 @@
 
 (setopt sentence-end-double-space nil)
 
-(setq use-package-always-ensure t)
-
-;; Misc minimizing emacs
+;; ----------------------- BASIC SETTINGS ----------------------- ;;
 (global-unset-key (kbd "C-z"))
-
 (setq make-backup-files nil)
 
-;; Who I am
 (setopt user-full-name "Arthur Mosley")
 (setopt user-mail-address "arthurcharlesmosley@gmail.com")
 
-;; Let's get a backtrace when errors are
 (setopt debug-on-error t)
-
-;; Display byte-compiler warnings on error
 (setopt byte-compile-debug t)
-
-;; Stop polluting the directory with auto-saved files and backup
 (setopt auto-save-default nil)
 (setopt make-backup-files nil)
 
-(set-frame-font "Menlo 15" nil t)
-
-;; Well, it's more so that you know this option
 (setopt kill-whole-line t)
 (setopt kill-read-only-ok t)
 (setopt require-final-newline 'visit)
 
-;; Scrolling done right
 (setopt scroll-error-top-bottom t)
 (setopt focus-follows-mouse t)
 (setopt recenter-positions '(top bottom middle))
 (scroll-bar-mode -1)
 
-;; Always use "y" for "yes"
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;; Enabling and disabling some modes
-;; Less is more - see https://bzg.fr/en/emacs-strip-tease/
 (auto-insert-mode 1)
 (tooltip-mode -1)
 (blink-cursor-mode -1)
 (pixel-scroll-mode 1)
 (tool-bar-mode -1)
-
 (delete-selection-mode 1)
 
-;; better scrolling
-(use-package good-scroll
-  :config
-  (good-scroll-mode 1))
-
-;; Default Frame
 (setopt default-frame-alist
-	'((menu-bar-lines . 0)
-	  (tool-bar-lines . 0)))
+    '((menu-bar-lines . 0)
+      (tool-bar-lines . 0)
+      (internal-border-width . 16)
+      (undecorated . t)))
 
-;; Don't display initial messages
 (setopt initial-scratch-message "")
 (setopt initial-major-mode 'org-mode)
 (setopt inhibit-startup-screen t)
@@ -135,10 +144,11 @@
 
 (save-place-mode 1)
 
+;; ----------------------- IBUFFER ----------------------- ;;
 (use-package ibuffer
   :config
   (setq ibuffer-expert t)
-  (setq -ibuffer-display-summary nil)
+  (setq ibuffer-display-summary nil)  ;; Fixed typo here
   (setq ibuffer-user-other-window nil)
   (setq ibuffer-show-empty-filter-groups nil)
   (setq ibuffer-default-sorting-mode 'filename/process)
@@ -157,71 +167,69 @@
                 (name 16 -1)
                 " " filename)))
   (setq ibuffer-saved-filter-groups
-	'(("Main"
-	   ("Directories" (mode . dired-mode))
-	   ("Clojure" (or
-		       (mode . clojure-ts-mode)
-		       (mode . clojure-mode)
-		       (mode . cider-mode)))
-	   ("Python"
-	    (mode . python-ts-mode))
-	   ("Fundamental" (or
-			   (mode . fundamental-mode)
-			   (mode . text-mode)))
-	   ("Emacs" (or
-                     (mode . emacs-lisp-mode)
-                     (name . "^\\*Help\\*$")
-                     (name . "^\\*Custom.*")
-                     (name . "^\\*Org Agenda\\*$")
-                     (name . "^\\*info\\*$")
-                     (name . "^\\*scratch\\*$")
-                     (name . "^\\*Backtrace\\*$")
-                     (name . "^\\*Messages\\*$"))))))
+    '(("Main"
+       ("Directories" (mode . dired-mode))
+       ("Clojure" (or
+               (mode . clojure-ts-mode)
+               (mode . clojure-mode)
+               (mode . cider-mode)))
+       ("Python"
+        (mode . python-ts-mode))
+       ("Fundamental" (or
+               (mode . fundamental-mode)
+               (mode . text-mode)))
+       ("Emacs" (or
+                      (mode . emacs-lisp-mode)
+                      (name . "^\\*Help\\*$")
+                      (name . "^\\*Custom.*")
+                      (name . "^\\*Org Agenda\\*$")
+                      (name . "^\\*info\\*$")
+                      (name . "^\\*scratch\\*$")
+                      (name . "^\\*Backtrace\\*$")
+                      (name . "^\\*Messages\\*$"))))))
   :hook
   (ibuffer-mode . (lambda ()
-		    (ibuffer-switch-to-saved-filter-groups "Main"))))
+            (ibuffer-switch-to-saved-filter-groups "Main"))))
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
-
-;; ----------------------- QOL Packages  ----------------------- ;;
-
+;; ----------------------- QOL PACKAGES ----------------------- ;;
 (use-package which-key
   :ensure t
   :config (which-key-mode))
 
-;; pick the specific window to go to.
 (use-package ace-window
   :bind ("C-x o" . ace-window))
 
-;; Remember the packages.
 (use-package winner
   :config
   (winner-mode 1))
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-
-(use-package zenburn-theme
-  :config
-  (load-theme 'zenburn t))
-
-;; Show line and column on the modeline.
 (setopt line-number-mode t)
 (setopt column-number-mode t)
 
-(use-package all-the-icons :ensure t)
+(use-package nerd-icons :ensure t)
+
+(use-package nerd-icons-dired
+  :hook (dired-mode . nerd-icons-dired-mode))
+
+(use-package nerd-icons-corfu
+  :after corfu
+  :init (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+
+(use-package nerd-icons-ibuffer
+  :hook (ibuffer-mode . nerd-icons-ibuffer-mode))
 
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
   :custom
-  (doom-modeline-height 25)     ; Sets the bar height
-  (doom-modeline-bar-width 4)   ; Sets the vertical bar thickness
-  (doom-modeline-env-version t) ; Shows Python/Clojure versions
-  (doom-modeline-lsp t))        ; Shows Eglot status
+  (doom-modeline-height 25)
+  (doom-modeline-bar-width 4)
+  (doom-modeline-env-version t)
+  (doom-modeline-lsp t))
 
-;; ----------------------- Completion  Packages  ----------------------- ;;
-;; come back to this for better completion.
+;; ----------------------- COMPLETION PACKAGES ----------------------- ;;
 (use-package vertico
   :init
   (vertico-mode))
@@ -230,24 +238,21 @@
   :init
   (global-corfu-mode)
   :custom
-  ;; Enable auto completion and configure quitting
   (corfu-auto t)
   (corfu-quit-no-match 'separator))
 
 (use-package consult
-  ;; Optional: set a few convenient keybindings up front
-  :bind (("C-s" . consult-line)                ;; better search
-         ("M-y" . consult-yank-pop)            ;; browse kill-ring
+  :bind (("C-s" . consult-line)
+         ("M-y" . consult-yank-pop)
          ("C-x b" . consult-buffer)
-	 :map project-prefix-map
-	 ("s" . consult-ripgrep)))
+     :map project-prefix-map
+     ("s" . consult-ripgrep)))
 
 (use-package embark
-  :bind (("C-." . embark-act)         ;; main action menu
-         ("C-;" . embark-dwim)        ;; smart action
-         ("C-h B" . embark-bindings)) ;; what’s bound here?
+  :bind (("C-." . embark-act)
+         ("C-;" . embark-dwim)
+         ("C-h B" . embark-bindings))
   :init
-  ;; show more context in the minibuffer when using embark
   (setq prefix-help-command #'embark-prefix-help-command))
 
 (use-package embark-consult
@@ -255,23 +260,21 @@
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
-;; Make xref results nicer
 (with-eval-after-load 'consult
   (setq xref-show-xrefs-function       #'consult-xref
         xref-show-definitions-function #'consult-xref))
 
 (use-package marginalia
   :init
-  (marginalia-mode)                       ; enable everywhere
+  (marginalia-mode)
   :bind (:map minibuffer-local-map
-              ("M-A" . marginalia-cycle)) ; toggle annotations
+              ("M-A" . marginalia-cycle))
   :custom
   (marginalia-annotators
    '(marginalia-annotators-heavy
      marginalia-annotators-light
      nil)))
 
-;; dired file manager
 (use-package dired
   :ensure nil
   :commands (dired)
@@ -292,33 +295,38 @@
   :custom
   (completion-styles '(orderless basic)))
 
-;; ensure that universally, meta is on command and super is on alt across any OS.
+;; macOS key mappings
 (when (eq system-type 'darwin)
   (setq x-meta-keysym 'super
         x-super-keysym 'meta))
 
-;; PROGRAMMING SETUP ;;
-(use-package dash)
+;; ----------------------- PROGRAMMING SETUP ----------------------- ;;
 
-;; python environment stuff
 (use-package envrc
   :ensure t
   :hook (after-init . envrc-global-mode))
 
-(use-package apheleia
-  :ensure t
-  :config
-  (apheleia-global-mode +1))
+(add-to-list 'display-buffer-alist
+             '("^\\*eldoc\\*"
+               (display-buffer-in-side-window)
+               (side . bottom)
+               (slot . 1)
+               (window-height . 0.2)))
 
 (keymap-global-set "C-c C" #'compile)
+
+(use-package hungry-delete
+  :ensure t
+  :hook (prog-mode . hungry-delete-mode)
+  :config
+  (setq hungry-delete-chars-to-skip " \t\r\f\v"))
 
 (use-package smartparens
   :init (require 'smartparens-config)
   :hook ((prog-mode . smartparens-mode)
-	 (prog-mode . show-smartparens-mode)
-	 (clojure-ts-mode . smartparens-strict-mode)
-	 (cider-repl-mode . smartparens-strict-mode)
-	 (emacs-lisp-mode . smartparens-strict-mode))
+     (clojure-ts-mode . smartparens-strict-mode)
+     (cider-repl-mode . smartparens-strict-mode)
+     (emacs-lisp-mode . smartparens-strict-mode))
   :bind (:map smartparens-mode-map
               ;; Movement
               ("C-M-f" . sp-forward-sexp)
@@ -341,7 +349,9 @@
               ;; Kill/Copy
               ("C-M-k" . sp-kill-sexp)
               ("C-M-w" . sp-copy-sexp)
-              ("C-k"   . sp-kill-hybrid-sexp))
+              ("C-k"   . sp-kill-hybrid-sexp)
+	      ;; transpose lines
+	      ("M-<up>" . sp-transpose-sexp))
   :config
   (with-eval-after-load 'smartparens
     (sp-local-pair 'clojure-mode "(" ")" :when '(sp-in-code-p)))
@@ -350,7 +360,11 @@
         sp-autoskip-closing-pair 'always
         sp-cancel-autoskip-on-backward-movement nil
         sp-navigate-consider-symbols t)
+  (sp-local-pair '(clojure-mode clojure-ts-mode) "`" nil :actions nil)
   (sp-pair "'" nil :actions nil))
+
+(use-package aggressive-indent
+  :hook (clojure-ts-mode . aggressive-indent-mode))
 
 (use-package project
   :ensure nil
@@ -364,18 +378,24 @@
   (define-key flymake-mode-map (kbd "M-d") 'flymake-show-buffer-diagnostics)
   (define-key flymake-mode-map (kbd "M-P") 'flymake-show-project-diagnostics))
 
-(setq c-default-style "stroustrup"
-      c-basic-indent 4
-      c-basic-offset 4)
-(c-set-offset 'innamespace 0)
-
+;; ----------------------- PYTHON SETUP (with IPython) ----------------------- ;;
 (use-package python
   :ensure nil
   :mode ("\\.py\\'" . python-ts-mode)
   :hook
-  ;; Remove the legacy backend. Eglot will handle the linting.
-  (python-ts-mode . (lambda ()
-                      (remove-hook 'flymake-diagnostic-functions 'python-flymake t))))
+  ((python-ts-mode . (lambda ()
+                       ;; Disable python-flymake, use your LSP for linting
+                       (remove-hook 'flymake-diagnostic-functions 'python-flymake t)))
+   ;; Enable Corfu in IPython REPL, disable auto-popup for manual control
+   (inferior-python-mode . (lambda ()
+                             (corfu-mode 1)
+                             (setq-local corfu-auto nil))))
+  :custom
+  ;; Use IPython as the REPL
+  (python-shell-interpreter "ipython")
+  (python-shell-interpreter-args "--simple-prompt -i")
+  ;; Keep native completion enabled
+  (python-shell-completion-native-enable t))
 
 (use-package python-black
   :demand t
@@ -392,15 +412,9 @@
     (when-let* ((root (ignore-errors (project-root (project-current))))
                 (cand (mapcar (lambda (d) (expand-file-name d root)) '(".venv" "venv")))
                 (venv (seq-find #'file-directory-p cand)))
-      (pyvenv-activate venv)))) ;; Fixed: was 'v', changed to 'venv'
+      (pyvenv-activate venv))))
 
-(use-package haskell-mode
-  :ensure t
-  :mode "\\.hs\\'"
-  :config
-  (add-hook 'haskell-mode-hook 
-            (lambda () (add-hook 'before-save-hook #'eglot-format-buffer nil t))))
-
+;; ----------------------- EGLOT (LSP) ----------------------- ;;
 (use-package eglot
   :ensure nil
   :defer t
@@ -414,26 +428,24 @@
          ("C-c c o" . eglot-code-actions-organize-imports)
          ("C-c c r" . eglot-rename)
          ("C-c c f" . eglot-format))
-  ;; Consolidation of all your programming hooks
-  :hook (((clojure-ts-mode python-ts-mode c++-ts-mode c-mode 
-			   typescript-ts-mode tsx-ts-mode css-mode html-mode haskell-mode) . eglot-ensure))
+  ;; Note: Removed clojure-ts-mode - CIDER handles that
+  :hook (((python-ts-mode c++-ts-mode c-mode
+              typescript-ts-mode tsx-ts-mode css-mode html-mode haskell-mode) . eglot-ensure))
   :config
-  ;; Refined server list
   (setq eglot-server-programs
         (append '(((typescript-ts-mode tsx-ts-mode) . ("typescript-language-server" "--stdio"))
                   (html-ts-mode . ("vscode-html-language-server" "--stdio"))
                   (css-ts-mode  . ("vscode-css-language-server" "--stdio"))
                   (haskell-mode . ("haskell-language-server-wrapper" "--stdio"))
-                  (python-ts-mode . ("basedpyright-langserver" "--stdio" "pyright-langserver" "pylsp"))
-                  ((clojure-mode clojure-ts-mode clojurescript-mode clojurec-mode) . ("clojure-lsp")))
+                  (python-ts-mode . ("basedpyright-langserver" "--stdio")))
                 eglot-server-programs))
   :custom
   (eglot-autoshutdown t)
   (eglot-confirm-server-initiated-edits nil)
-  ;; Performance: Silence heavy logging to prevent UI lag
   (eglot-events-buffer-config '(:size 0 :format full))
   (eglot-sync-connect nil))
 
+;; ----------------------- CLOJURE SETUP ----------------------- ;;
 (use-package clojure-ts-mode
   :mode (("\\.clj\\'"  . clojure-ts-mode)
          ("\\.cljs\\'" . clojure-ts-mode)
@@ -445,6 +457,7 @@
   (defun my/clojure-local-setup ()
     (electric-pair-local-mode -1)))
 
+;; Configure eldoc to show both CIDER and other documentation
 (setq eldoc-documentation-strategy 'eldoc-documentation-compose)
 (setq cider-eldoc-display-context-dependent-info nil)
 
@@ -463,27 +476,57 @@
         cider-repl-pop-to-buffer-on-connect 'display-only
         cider-clojure-cli-aliases ":dev"
         cider-repl-display-result t
-	cider-eldoc-display-for-symbol-at-point nil
-	cider-eldoc-display-context-dependent-info nil)
+        cider-eldoc-display-for-symbol-at-point nil
+        cider-eldoc-display-context-dependent-info nil)
   :hook ((cider-repl-mode . eldoc-mode))
   :config
   (with-eval-after-load 'clojure-ts-mode
     (define-key clojure-ts-mode-map (kbd "C-c r") #'cider-integrant-reset)))
 
-;; BASIC ORG MODE SETUP JUST FOR NOTE TAKING THAT IS ALL
+(use-package haskell-mode
+  :ensure t
+  :mode "\\.hs\\'")
 
+;; ----------------------- ORG MODE ----------------------- ;;
 (setq inhibit-splash-screen t)
-
 (transient-mark-mode 1)
 
 (require 'org)
+(setq org-directory "~/org/")
+(setq org-default-notes-file (concat org-directory "refile.org"))
+(global-set-key (kbd "C-c c") 'org-capture)
 
-;; magit setup
+(use-package olivetti
+  :custom (olivetti-body-width 90)
+  :hook (org-mode . olivetti-mode))
 
+(setq org-hide-emphasis-markers t)    ; hide *bold* markers, show just bold
+(setq org-pretty-entities t)          ; \alpha → α, \to → →
+(setq org-ellipsis " ▾")             ; nicer fold indicator
+
+(use-package org-modern
+  :hook (org-mode . org-modern-mode)
+  :custom
+  (org-modern-star '("◉" "○" "✸" "✿"))
+  (org-modern-table t)
+  (org-modern-block-fringe t))
+
+(setq org-capture-templates
+      '(("w" "Workout Log" entry (file+datetree "~/org/workouts.org")
+         "* %^{Workout Type|Push|Pull|Legs A (Quads)|Legs B (Hams)}\n:PROPERTIES:\n:TIME: %U\n:END:\n%?" 
+         :empty-lines 1)))
+
+(add-to-list 'org-capture-templates
+             '("n" "Daily Nutrition" table-line 
+               (file+olp "~/org/nutrition.org" "Logs" "Current Week")
+               "| %U | %^{Calories} | %^{Protein} | %^{Carbs} | %^{Fats} | %^{Notes} |"
+               :immediate-finish t))
+
+;; ----------------------- GIT (MAGIT) ----------------------- ;;
 (use-package magit
   :bind (("C-x g" . magit-status)))
 
-;; better terminal (vterm) setup
+;; ----------------------- TERMINAL (VTERM) ----------------------- ;;
 (use-package vterm
   :hook (vterm-mode . (lambda () (display-line-numbers-mode -1))))
 
@@ -496,6 +539,7 @@
 
 (global-set-key (kbd "C-c t w") 'vterm-new-window)
 
+;; ----------------------- FILE BROWSER (TREEMACS) ----------------------- ;;
 (use-package treemacs
   :config
   :bind
